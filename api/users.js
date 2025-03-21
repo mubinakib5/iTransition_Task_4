8db4303570bfdd91d97e59f097abab345e10229d
mubinakib5 <mubinalakib786@gmail.com>
2025-03-21 23:49:34 +0600

    Users.js Revamped Again

--- api/users.js
+++ api/users.js
@@ -5,7 +5,27 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
+    require: true,
   },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+// Add connection test
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+// Test the connection
+pool.connect((err, client, done) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+  } else {
+    console.log("Successfully connected to database");
+    done();
+  }
 });
 
 module.exports = async (req, res) => {
